fea393e6d9208a923a3db45676ade2dec78c1b9b
Anne-Laure Bienaimé <annelaure.bienaime@gmail.com>
2017-11-10 12:51:53 +0100

    Jack birthday

--- routes/index.js
+++ routes/index.js
@@ -23,20 +23,28 @@ router.get('/', function(req, res, next) {
 
 /* GET home page personnalisée par login */
 router.get('/homepage-:id(\\d+)', function(req, res, next) {
-	connection.query('SELECT * FROM person where id_p = ?', [req.params.id], function (error, results, fields) {
-		if (error) {
-			console.log(error);
-		}
-		res.render('homepage', {
-			title: 'Accueil - BWild !!',
-			user: '#username',
-			person : results[0]
+	let date = new Date();
+	connection.query('SELECT * FROM person', function (error, resultsbd, fields) {
+		connection.query('SELECT * FROM person where id_p = ?', [req.params.id], function (error, results, fields) {
+			connection.query('SELECT * FROM quote', function (error, resultsqt, fields) {	
+				if (error) {
+				console.log(error);
+				}
+				console.log(resultsbd);
+				console.log(results);
+				res.render('homepage', {
+					title: 'Accueil - BWild !!',
+					birthday : resultsbd.filter(function(valeur) {
+    					return (valeur.birthday.getDate() == date.getDate() &&  valeur.birthday.getMonth() == date.getMonth());
+					}),
+					
+					person : results[0],
+					quote : resultsqt
+				});	
+			});	
 		});
 	});
 });
-
-
-
 
 /* GET Wildmates */
 router.get('/profil', function(req, res, next) {
